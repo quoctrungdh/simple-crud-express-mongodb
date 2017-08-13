90fac6f8dd94f6d39cb5a90f7d2f8e9741fd52f0
quoctrungdh <quoctrungdh@gmail.com>
2017-08-13 22:54:46 +0700

    feat(API): add GET '/'

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('hello');
+app.get('/', (req, res) => {
+  // TODO: send css js
+  res.sendFile(path.resolve('build/index.html'));
 });
+module.exports = app;
